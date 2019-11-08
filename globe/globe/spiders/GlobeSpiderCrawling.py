cd58674a401be29f6611deec0c7feaf7a191a291
John C. Merfeld <john.merfeld@gmail.com>
2019-11-08 14:33:29 -0500

    globe/globe/spiders/GlobeSpiderCrawling.py
    New spider structure

--- globe/globe/spiders/GlobeSpiderCrawling.py
+++ globe/globe/spiders/GlobeSpiderCrawling.py
@@ -2,6 +2,24 @@ from datetime import datetime as dt
 from scrapy.spiders import Rule, CrawlSpider
 from scrapy.linkextractors import LinkExtractor
 from pymongo import MongoClient
+import re
+
+def getTags(s):
+    
+    neighborhoods = ['Allston', 'Brighton', 'Back Bay', 'North End', 'Roxbury',
+                     'Bay Village', 'Beacon Hill', 'Charlestown', 'Chinatown',
+                     'Dorchester', 'Downtown', 'East Boston', 'Kenmore', 'Fenway',
+                     'Hyde Park', 'Jamaica Plain', 'Mattapan', 'Mission Hill',
+                     'Roslindale', 'South Boston', 'South End', 'West End', 
+                     'West Roxbury', ]   
+    
+    tags = []
+    
+    for neighborhood in neighborhoods:
+        if bool(re.search(neighborhood, s)):
+            tags.append(neighborhood)
+    
+    return tags
 
 class MongoDB:
     """
@@ -38,6 +56,7 @@ class GlobeSpiderCrawler(CrawlSpider):
 
     def parse_items(self, response):
         items = []
+        tags = []
 
         for paragraph in response.xpath('//div[@class="article-text"]/p'):
             try:
@@ -46,14 +65,32 @@ class GlobeSpiderCrawler(CrawlSpider):
             except:
                 pass
             
-            
+        # tag and clean the story
+        flat_items = [item for sublist in items for item in sublist]
+        story = " ".join(flat_items)
+        story = story.replace('\n\t ','')
+        story = story.replace('\t ','')
+        story = story.replace('\n ','')
+        
+        
+        tags = getTags(story)
+        
 
         if len(items) > 0:
-            #db = self.db_client.get_client('globe_stories_db')
+
             db = self.db_client.get_client(self.db_name)
-            #collection = db['globe_stories_collection']
-            collection = db[self.collection_name]
             timestamp = response.meta['wayback_machine_time'].timestamp()
-            document = {'timestamp': timestamp, 'items': items}
-            result = collection.insert_one(document)
+
+            collection = db[self.collection_name]
+            document = {'timestamp': timestamp, 'story': story}
+            
+            count = collection.count_documents({'story': {'$in': [story]}})
+            # primitive duplicate handling
+            if count == 0:
+                
+                collection.insert_one(document)
+                for tag in tags:
+                    collection_by_tag = db[tag.lower().replace(" ", "_")]
+                    collection_by_tag.insert_one(document)
+            
             return document
--- textAnalysis.js
+++ textAnalysis.js
@@ -1,37 +1,48 @@
-var city_list = ['Boston', 'Jamaica']
 
-// warning, this does not work correctly but it might do something partially correctly
 var map = function() {
-    var stories = this.items;
-    if (stories) {
-      for (var story = 0; story < stories.length; i++) {
-        var keys = [];
-        for (var sentence = 0; sentence < stories[story].length; j++) {
-          words = stories[story][sentence].split(/\W+/);
-          for (var k = 0; k < words.length; k++) {
-            if (words[k])  {
-              if (city_list.includes(words[k]) {
-                keys.push(words[k])
-              }
-              emit(words[k], 1);
-            }
-          }
-        }
+  var story = this.story;
+
+  if (story) {
+    words = story.split(" ");///\W+/);
+    for (var i = 0; i < words.length; i++) {
+      if (words[i]) {
+        emit(words[i], 1);
       }
     }
+  }
 };
 
 var reduce = function(key, values ) {
-    var count = 0;
-    values.forEach(function(v) {
-        count += v;
-    });
-    return count;
+
+  var count = 0;
+
+  values.forEach(function(v) {
+    count += v
+  })
+  return count;
 }
 
 // full thing
-// db.support_emails.mapReduce(map, reduce, { out: "word_count" })
+db.globe_stories13.mapReduce(map, reduce, { out: "word_count3" })
 
 // subset
-db.globe_stories.mapReduce(map, reduce, {limit: 1000, out: "word_count" })
-db.word_count.find().sort({ value: -1 }).limit(10)
+//db.globe_stories12.mapReduce(map, reduce, {limit: 1000, out: "word_count" })
+db.word_count3.find()//.sort({ value: -1 }).limit(100)
+
+db.getCollectionNames().forEach(function(col) {
+  db.col.findOne()
+  db.col.mapReduce(map, reduce, { out: "wc_" + String.valueOf(col)})
+})
+
+db.getCollectionNames().forEach(function(col) {
+  print(col)
+})
+
+db.getCollectionNames().forEach(function(col) {
+  try {
+    db[col].mapReduce(map, reduce, { out: "wc_" + col})
+  }
+  catch(err) {
+    print(err)
+  }
+})
