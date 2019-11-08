019bff7830edb9a16278e7f2b9253de762acc112
John C. Merfeld <john.merfeld@gmail.com>
2019-11-08 14:37:37 -0500

    text analysis

--- textAnalysis.js
+++ textAnalysis.js
@@ -1,9 +1,10 @@
-
+// The map function reads the text and creates a bunch of intermediate key-value files.
+// In this case, the key is the word and the value is '1'
 var map = function() {
   var story = this.story;
 
   if (story) {
-    words = story.split(" ");///\W+/);
+    words = story.split(/\W+/);
     for (var i = 0; i < words.length; i++) {
       if (words[i]) {
         emit(words[i], 1);
@@ -12,34 +13,22 @@ var map = function() {
   }
 };
 
+// The reduce function takes every key-value pair with a given key and
+// adds up their values (in this case, it's a count. So each instance of a word
+// has value '1' -- so the result will be a word count)
 var reduce = function(key, values ) {
 
   var count = 0;
 
   values.forEach(function(v) {
-    count += v
+    count += v;
   })
   return count;
 }
 
-// full thing
-db.globe_stories13.mapReduce(map, reduce, { out: "word_count3" })
-
-// subset
-//db.globe_stories12.mapReduce(map, reduce, {limit: 1000, out: "word_count" })
-db.word_count3.find()//.sort({ value: -1 }).limit(100)
-
+// We want to do a seperate word count for each neighborhood
 db.getCollectionNames().forEach(function(col) {
-  db.col.findOne()
-  db.col.mapReduce(map, reduce, { out: "wc_" + String.valueOf(col)})
-})
-
-db.getCollectionNames().forEach(function(col) {
-  print(col)
-})
-
-db.getCollectionNames().forEach(function(col) {
-  try {
+  try { // run our map function, our reduce function, and write the results to a unique word count output table
     db[col].mapReduce(map, reduce, { out: "wc_" + col})
   }
   catch(err) {
